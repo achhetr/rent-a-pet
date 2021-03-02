c16bc90a59b2f3b0dae93a57c4ecf7d9aaa265f0
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-02 16:23:48 +1100

    Added new and create method for bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :set_cocktail, only: [:create, :new]
+
+  def def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Book.new(booking_params)
+    @booking.pet = @pet
+    if @booking.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def set_booking
+    @booking = Pet.find(params[:pet_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :total_price, :user_id, :pet_id)
+  end
+  
 end
