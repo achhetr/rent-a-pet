e25cb702f780c88636da8e3f3484438f1b7e6a96
akash chhetri <akash.chhetri1@gmail.com>
2021-03-02 12:53:44 +1100

    type column removed from User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_003051) do
+ActiveRecord::Schema.define(version: 2021_03_02_015251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_003051) do
     t.string "mobile"
     t.string "location"
     t.string "image_url"
-    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
