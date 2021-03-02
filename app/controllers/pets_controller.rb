f6059b9fd5901c6a8a1305d58f943a57ab8e79c5
iantaylor <iantaylor03@gmail.com>
2021-03-02 14:37:25 +1100

    pets controller updated with index and destroy method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,14 @@
 class PetsController < ApplicationController
+    def index
+    @pets = Pets.all
+            
+    end
+    
+    def destroy
+        @pets = Pets.find(parmas[:pet_id])
+        @pets.destroy
+        redirect_to pets_path
+    end
+
+  
 end
