2e8a382a3e7beb83b221d8cb7c079fb2851401b9
akash chhetri <akash.chhetri1@gmail.com>
2021-03-02 11:51:45 +1100

    pets, bookings and dashboards controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do 
-    resources :bookings only: [:new, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings only: [:index, :show]
-  resources :dashboard only: [:index]
+  resources :bookings, only: [:index, :show]
+  resources :dashboard, only: [:index]
 end
