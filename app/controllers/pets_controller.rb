b712f9dcfc6e4862fc4baedcd013fdec8d707d43
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-02 14:30:10 +1100

    Created the pet edit and update method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,13 @@
 class PetsController < ApplicationController
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+  
 end
