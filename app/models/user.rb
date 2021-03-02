4a0bff317c70f7fd8c42a573e22280f790bb2555
akash chhetri <akash.chhetri1@gmail.com>
2021-03-02 13:01:09 +1100

    validation in user, pet and booking added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pets, dependent: :destroy
   has_many :bookings
+
+  validates :name, presence: true
+  validates :location, presence: true
 end
