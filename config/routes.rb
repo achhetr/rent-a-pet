2ea6b3bb1b104a300ad63ff751f84029cb5ff15c
akash chhetri <akash.chhetri1@gmail.com>
2021-03-02 11:44:33 +1100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # root to: "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pets do 
+    resources :bookings only: [:new, :create]
+  end
+  resources :bookings only: [:index, :show]
+  resources :dashboard only: [:index]
 end
