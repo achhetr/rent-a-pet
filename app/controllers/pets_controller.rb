8e183f23b7c0498eacf9011fd52743067df5627b
akash chhetri <akash.chhetri1@gmail.com>
2021-03-02 14:29:14 +1100

    new, create, show and pet_params permitted

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,24 @@
 class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+  
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
   def show
     @pet = Pet.find(params[:id])
+  end
+
+  private 
+
+  def pet_params
+    params.require(:pet).permit(:name, :description, :type, :image_url, :price)
   end
 end
