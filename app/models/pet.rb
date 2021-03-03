4f29e28a32f438affc7369e4e1f6c6c481ddc394
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-03 15:01:44 +1100

    Active storage

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :user_id, presence: true
   validates :name, presence: true
   validates :price, presence: true
