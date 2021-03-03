e3574e6fc28f4eff3fc51e27f294719cbf3ee79f
akash chhetri <akash.chhetri1@gmail.com>
2021-03-03 15:09:22 +1100

    dependat destory booking aded in pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   validates :user_id, presence: true
   validates :name, presence: true
