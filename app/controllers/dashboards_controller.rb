5ab918deede57ce1ff3e1526c91b64aee67cbe49
akash chhetri <akash.chhetri1@gmail.com>
2021-03-03 14:56:30 +1100

    my offers filtered

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,10 @@ class DashboardsController < ApplicationController
 
   def index
     @pets = current_user.pets
-    @booking_offers = current_user.bookings
+    # @booking_offers = current_user.bookings
+    @booking_offers = Booking.all.select do |booking|
+      booking.pet.user.id == current_user.id
+    end 
     @my_bookings = Booking.where(user_id: current_user.id)
   end
 
