c4a99f5d6fa5d8735051ef0b40a821a4c0c3e014
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-03 11:53:00 +1100

    Added an index method for dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,9 @@
 class DashboardsController < ApplicationController
+
+  def index
+    @user = current_user 
+    @pets = Pet.where(user_id = @user :id)
+    @bookings = Booking.where(user_id = @user :id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show]
-  resources :dashboard, only: [:index]
+  resources :dashboards, only: [:index]
 end
