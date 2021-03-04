6478cfafac56f7dd328dd2c2114b2ea198dfe358
akash chhetri <akash.chhetri1@gmail.com>
2021-03-04 12:03:42 +1100

    user location converted to latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_000819) do
+ActiveRecord::Schema.define(version: 2021_03_04_010259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_000819) do
     t.string "mobile"
     t.string "location"
     t.boolean "has_pet"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
