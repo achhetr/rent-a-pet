63a2eb76ec8cd15044964d73882023940e040a57
akash chhetri <akash.chhetri1@gmail.com>
2021-03-04 12:49:46 +1100

    routes and controller added for profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pages, only: [:edit, :update]
   resources :pets do 
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :destroy, :update]
   resources :dashboards, only: [:index]
+  resources :profiles, only: [:update]
 end
