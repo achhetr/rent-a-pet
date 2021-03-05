2e0a5311b92e1987e5b319c55ae695bc1bec762f
akash chhetri <akash.chhetri1@gmail.com>
2021-03-05 15:28:02 +1100

    browser location added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const fitMapToMarkers = (map, markers) => {
 	const bounds = new mapboxgl.LngLatBounds();
 	markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-	map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+	map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
 };
 
 const addMarkersToMap = (map, markers) => {
